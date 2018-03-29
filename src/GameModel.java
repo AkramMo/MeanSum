00f37249ff6ffa6499a8defcbc6338cc869bf983
Akram Mo <Akram-mo@hotmail.com>
2018-03-28 21:28:56 -0400

    Biensure

--- src/GameModel.java
+++ src/GameModel.java
@@ -13,11 +13,15 @@ public class GameModel {
 	// TODO Add attributes (list of numbers, etc.)
 
 	private ArrayList<Integer> listNumber;
+
+	private boolean[] etatSelection;
 	// TODO Implement constructor and methods (generation of a game, etc.)
 
 	public GameModel() {
 
 		listNumber = new ArrayList<Integer>();
+		etatSelection = new boolean[12];
+		initArray(etatSelection);
 	}
 
 	/**
@@ -30,10 +34,10 @@ public class GameModel {
 	 */
 	private int getRandom(int min, int max) {
 
-		// Vérifie que le min est plus petit que le max
+		// Vï¿½rifie que le min est plus petit que le max
 		if(min >= max) {
 
-			throw new IllegalArgumentException("Le minimum est plus grand ou égal au maxium !");
+			throw new IllegalArgumentException("Le minimum est plus grand ou ï¿½gal au maxium !");
 		}
 
 		Random rand = new Random();
@@ -44,9 +48,17 @@ public class GameModel {
 
 	}
 
+	private void initArray(boolean[] arraySelection) {
+
+		for(int i = 0; i < getDigits().length(); i++) {
+
+			arraySelection[i] = false;
+		}
+	}
+
 	/**
-	 * Méthode qui remplie l'Attribut listNumber
-	 * selon certaines probabilité stricte aux critères
+	 * Mï¿½thode qui remplie l'Attribut listNumber
+	 * selon certaines probabilitï¿½ stricte aux critï¿½res
 	 * du jeux MeanSum.
 	 */
 	public void generateGame() {
@@ -68,8 +80,8 @@ public class GameModel {
 
 	/**
 	 * Fonctions qui retourne une chaine
-	 * de caractère avec tout les nombres
-	 * générés concaténés
+	 * de caractï¿½re avec tout les nombres
+	 * gï¿½nï¿½rï¿½s concatï¿½nï¿½s
 	 * @return
 	 */
 	public String getDigits() {
@@ -83,12 +95,81 @@ public class GameModel {
 		return chaineCaractere;
 	}
 
-/*	public static void main(String[] args) {
+	public boolean selectionValide(int ps1, int ps2) {
+
+		String nbrSelected = "";
+		boolean selValide = false;
+
+		if(ps1 <  ps2 && ps1 >= 0 && ps2 < (getDigits().length() - 1)) {
+
+			for(int i = ps1; i < ps2; i++) {
+
+				if(listNumber.get(i) > 9 ) {
+
+					nbrSelected += listNumber.get(i);
+					i++;
+
+				}else {
+
+					nbrSelected += listNumber.get(i);
+				}
+			}
+
+			if(listNumber.contains(Integer.parseInt(nbrSelected))){
+
+				selValide = true;
+			}
+
+		}
+
+		return selValide;
+
+	}
+
+	public void changeState(int ps1, int ps2) {
+
+		if(ps1 < ps2 && ps2 < getDigits().length()) {
+
+			for(int i = ps1; i < ps2; i++) {
+
+				etatSelection[i] = true;
+
+			}	
+		}
+	}
+
+	public int currentSum(){
+
+		String nbrSum = "";
+		int sum = 0;
+		char[] allNumbers = getDigits().toCharArray();
+
+		for(int i = 0; i < etatSelection.length; i++) {
+
+
+			if(etatSelection[i]) {
+
+				nbrSum += String.valueOf(allNumbers[i]);
+				if(i != 11) {
+					if(!etatSelection[i+1]) {
+
+						sum = sum + Integer.parseInt(nbrSum);
+						nbrSum = "";
+					}
+				}
+			}
+
+		}
+		
+		return sum;
+
+	}
+	/*	public static void main(String[] args) {
 
 		GameModel testMethode = new GameModel();
 
 		testMethode.generateGame();
 		System.out.println(testMethode.getDigits());
 	}
-*/
+	 */
 }
